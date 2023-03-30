f25de52fc76665735d63b2499e94b648da274a17
Esakki-raj <126795813+Esakki-raj@users.noreply.github.com>
2023-03-30 13:21:38 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Esakki Raj (Nokia)
    # Additional Authors:
-   # <your GitHub username>
+   # Esakki-raj
 
    # Input:
    # p, principal amount
